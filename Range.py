46e4280c0f78eabdb28fdf99e0cfa64db36200c2
DKD07 <161266358+DKD07@users.noreply.github.com>
2024-03-06 21:45:08 +0530

    Add files via upload

---
+++ Range.py
@@ -0,0 +1,8 @@
+for k in range(5):
+    print(k+1)
+print("*****")
+for k in range(1,8):
+    print(k)
+print("*****")
+for k in range(1,8,2):  #jump to 2steps directly
+    print(k)
---
+++ for-loops.py
@@ -0,0 +1,14 @@
+#Example-1
+name="Dinesh"
+for i in name:
+    print(i)
+    if(i=='n'):
+        print("This is something special!")
+print("THANK YOU")
+#Example-2
+colors=["Red","Green","Blue","Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+        
